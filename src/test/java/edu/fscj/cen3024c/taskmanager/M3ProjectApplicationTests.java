1726d498f77b5721c87e82589341db6a55ebf835
Singletary <david.singletary@fscj.edu>
2025-01-21 16:37:22 -0500

    tweaked pom

--- src/test/java/edu/fscj/cen3024c/taskmanager/M3ProjectApplicationTests.java
+++ src/test/java/edu/fscj/cen3024c/taskmanager/M3ProjectApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class M2ProjectApplicationTests {
+class M3ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
